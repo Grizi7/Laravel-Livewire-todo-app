e1751dcd6550f2418dcdeb9b267d79ba605e60c4
Grizi7 <mahmoudzidan7717@gmail.com>
2024-05-18 21:35:16 +0300

    installing and setting up laravel/breeze

--- todo/vite.config.js
+++ todo/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: "public/build",
+        manifest: true,
+        rollupOptions: {
+            input: "resources/js/app.js", // adjust this as necessary
+        },
+    },
 });
